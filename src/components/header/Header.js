4bc350c108a682880dec25c87893bfa0edd5e2d2
Hugo Silva <hugo.m.silva.023@gmail.com>
2022-11-21 00:51:16 -0300

    Criando componente Header

---
+++ src/components/header/Header.js
@@ -0,0 +1,45 @@
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../contexts/Authenticator'
+import React, { useContext } from 'react';
+
+import * as S from "./Header.styles"
+
+/** Importando as imagens */
+import Logo from '../../pages/home/images/Logo.png';
+import Chave from '../../pages/home/images/Chave.png';
+import Seta from '../../pages/home/images/Seta.png';
+
+
+const Header = () => {
+
+    const navigate = useNavigate();
+    const {name, setName} = useContext(AuthContext);
+
+    const logout = () => {
+        navigate("/");
+        localStorage.removeItem("token");
+        setName("")
+    }
+
+  return (
+    <div>
+        <div className="row justify-content-between pb-4">
+            <div className="col-5 display-block">
+                <img src={Logo} alt="Ioasys logo"/>
+                <span className="align-bottom d-xs-inline">
+                    <S.LogoBook>Books</S.LogoBook>
+                </span>
+            </div>
+            <div className="col-3">
+                <span className="d-none d-md-block d-lg-inline">Bem vindo, {name}! </span>
+                <S.LogoutButton onClick={logout}>
+                    <S.IconBack src={Chave} alt="Ioasys logo"/>
+                    <S.IconRight src={Seta} alt="Ioasys logo"/>
+                </S.LogoutButton>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header;
---
+++ src/components/header/Header.styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+// Component LogoBook
+export const LogoBook = styled.span`
+    margin-left: 16px;
+    font-size: 28px;
+    color: #333333;
+`;
+
+// Component LogoutButton
+export const LogoutButton = styled.button`
+    background: none;
+    border-radius: 100%;
+    border: 1px solid rgba(51, 51, 51, 0.2);
+    transform: matrix(1, 0, 0, 1, 0, 0);
+    margin-right: 2rem;
+`;
+
+// Component IconBack
+export const IconBack = styled.img`
+    width: 5.5px;
+    height: 14px;
+    margin-bottom: 0.3rem;
+`;
+
+// Component IconRight
+export const IconRight = styled.img`
+    margin-bottom: 0.3rem;
+`;
