8c2f5b84bb863a1bfea89135f31f978e3a2ac9aa
Hugo Silva <hugo.m.silva.023@gmail.com>
2022-11-25 14:22:16 -0300

    feat: aplicando a estrutura de styled components ao formAuthentication

---
+++ src/components/formAuthentication/FormAuthentication.jsx
@@ -0,0 +1,107 @@
+import React, { useState, useContext } from "react";
+import { AuthContext } from "../../contexts/Authenticator";
+import { useNavigate } from "react-router-dom";
+import api, { setAuthToken } from "../../services/api";
+
+import * as S from "./FormAuthentication.styles";
+import Logo from "../../pages/login/images/Logo.png";
+
+function FormAuthentication() {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("desafio@ioasys.com.br");
+  const [password, setPassword] = useState("12341234");
+  const [msg, setMsg] = useState(null);
+  const { setName } = useContext(AuthContext);
+
+  const Login = async () => {
+    const userData = {
+      email,
+      password,
+    };
+
+    async function getDataLogin() {
+      try {
+        const { data, headers } = await api.post("/auth/sign-in", userData);
+        const { authorization } = headers;
+
+        setAuthToken(authorization);
+
+        if (authorization) {
+          navigate("/Home");
+          localStorage.setItem("token", authorization);
+        }
+
+        setName(data.name);
+      } catch (error) {
+        const resp = JSON.parse(error.request.response);
+        const { message } = resp.errors;
+        setMsg(message);
+      }
+    }
+    getDataLogin();
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    Login();
+  }
+
+  return (
+    <S.Global>
+        <div className="container">
+            <div class="row align-items-end">
+                <div class="col-md-7 col-lg-5 col-sm-12">
+                <form onSubmit={handleSubmit}>
+                    <S.BoxLogo>
+                        <img src={Logo} alt="Ioasys logo" />
+                        <S.Book>Books</S.Book>
+                    </S.BoxLogo>
+                    <S.BoxEmail>
+                        <S.Box>
+                            <label htmlFor="email">
+                                <S.TitleBox>Email</S.TitleBox>
+                                <S.InputBox>
+                                    <input
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    />
+                                </S.InputBox>
+                            </label>
+                        </S.Box>
+                    </S.BoxEmail>
+                    <S.BoxPassword>
+                        <S.Box>
+                            <label htmlFor="password">
+                                <S.TitleBox>Senha</S.TitleBox>
+                                <S.InputBox>
+                                    <input
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    />
+                                    <S.Button>Entrar</S.Button>
+                                </S.InputBox>
+                            </label>
+                        </S.Box>
+                    </S.BoxPassword>
+                    {msg && (
+                    <S.MsgBox>
+                        <S.Box>{msg}</S.Box>
+                    </S.MsgBox>
+                    )}
+                </form>
+                </div>
+            </div>
+        </div>
+    </S.Global>
+  );
+}
+
+export default FormAuthentication;
--- src/components/formAuthentication/FormAuthentication.styles.js
+++ src/components/formAuthentication/FormAuthentication.styles.js
@@ -1,31 +1,38 @@
-@font-face {
+import styled from "styled-components";
+import Light from "../../fonts_heebo/Heebo-Light.ttf"
+import Medium from "../../fonts_heebo/Heebo-Medium.ttf"
+
+// Component Global - Fonts Heebo
+export const Global = styled.div`
+    @font-face {
     font-family: 'Heebo Light';
-    src: url('../../fonts_heebo/Heebo-Light.ttf') format('truetype');
-}
+    src: url(${Light}) format('truetype');
+    }
 
-@font-face {
-    font-family: 'Heebo Medium';
-    src: url('../../fonts_heebo/Heebo-Medium.ttf') format('truetype');
-}
+    @font-face {
+        font-family: 'Heebo Medium';
+        src: url(${Medium}) format('truetype');
+    }
+`
 
-.boxLogo{
+export const BoxLogo = styled.div `
     margin-top: 273px;
-}
+`
 
-.book{
+export const Book = styled.span `
     position: absolute;
     margin-left: 16px;
     top: 268px;
 
     font-size: 28px;
     color: #FFFFFF;
-}
+`
 
-.boxEmail{
+export const BoxEmail = styled.div `
     margin-top: 39px;
-}
+`
 
-.box{
+export const Box = styled.div `
     background: rgba(0, 0, 0, 0.32);
     border-radius: 4px;
     backdrop-filter: blur(1px);
@@ -33,34 +40,34 @@
     font-family: 'Heebo Light';
     font-style: normal;
     font-weight: 400;
-}
+`
 
-input{
+export const Input = styled.input `
     background: none;
 
     color: #FFFFFF;
     border: none;
-}
+`
 
-.titleBox {
+export const TitleBox = styled.p `
     line-height: 16px;
     padding-top: 8px;
     padding-left: 16px;
 
     opacity: 0.5;
-}
+`
 
-.inputBox {
+export const InputBox = styled.div `
     padding-top: 4px;
     padding-left: 16px;
     padding-bottom: 8px;
-}
+`
 
-.boxPassword {
+export const BoxPassword = styled.div `
     padding-top: 16px;
-}
+`
 
-.buttonBox {
+export const Button = styled.button`
     background: #FFFFFF;
     border-radius: 44px;
     border: none;
@@ -71,22 +78,21 @@ input{
     font-size: 16px;
     line-height: 20px; 
     color: #B22E6F;
-}
+`
 
-.msg {
+export const MsgBox = styled.div `
     display: inline-block;
     margin-top: 24px;
     border-radius: 4px;
 
     background: rgba(255, 255, 255, 0.4);
+`
 
-}
-
-.msg p {
+export const Msg = styled.span ` 
     padding: 16px 16px 0 16px;
     font-family: 'Heebo Medium';
     font-weight: 700;
     font-size: 16px;
     line-height: 16px;
     color: #FFFFFF;
-}
+`
